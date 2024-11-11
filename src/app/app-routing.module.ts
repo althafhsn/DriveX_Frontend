a25eb142b72eb875f9abaf34712e90962a178eff
Your NameHasniUT01624 <wingscer@gmail.com>
2024-11-11 16:15:28 +0530

    LangingPage final UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LandingPageComponent } from './components/Customer/landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  {path: 'LandingPage',component:LandingPageComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,37 @@ import { AddCarComponent } from './components/Admin/add-car/add-car.component';
 
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { LandingPageComponent } from './components/Customer/landing-page/landing-page.component';
+import { SearchComponent } from './components/Customer/search/search.component';
+import { BookingComponent } from './components/Customer/booking/booking.component';
+import { NavbarComponent } from './components/Customer/landing-page/navbar/navbar.component';
+import { SlideshowComponent } from './components/Customer/landing-page/slideshow/slideshow.component';
+import { SearchCarComponent } from './components/Customer/landing-page/search-car/search-car.component';
+import { BrandComponent } from './components/Customer/landing-page/brand/brand.component';
+import { CardComponent } from './components/Customer/landing-page/card/card.component';
+import { TypeComponent } from './components/Customer/landing-page/type/type.component';
+import { ScrollComponent } from './components/Customer/landing-page/scroll/scroll.component';
+import { FooterComponent } from './components/Customer/landing-page/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-       NavBarComponent,
+    NavBarComponent,
     SideBarComponent,
     ColorModePanelComponent,
-    AddCarComponent
+    AddCarComponent,
+    LandingPageComponent,
+    SearchComponent,
+    BookingComponent,
+    NavbarComponent,
+    SlideshowComponent,
+    SearchCarComponent,
+    BrandComponent,
+    CardComponent,
+    TypeComponent,
+    ScrollComponent,
+    FooterComponent
 
     
   ],
