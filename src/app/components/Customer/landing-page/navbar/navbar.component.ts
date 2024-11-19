e51654f798a4826654240bed10b8611bbc4341d1
althafhsn <althafhsn.c@gmail.com>
2024-11-20 01:09:09 +0530

    admin

--- src/app/components/Customer/landing-page/navbar/navbar.component.ts
+++ src/app/components/Customer/landing-page/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../services/auth.service';
+import { ApiService } from '../../../../services/api.service';
+import { UserStoreService } from '../../../../services/user-store.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  public users: any = [];
+
+  public fullName: string = "";
+  public role: string = "";
+  constructor(
+    private authService: AuthService,
+    private store: UserStoreService
+  ) { }
+
+  ngOnInit() {
+  
+  
+    // Fetch and set the user's full name
+    this.store.getFullName().subscribe({
+      next: (val) => {
+        const fullNameFromToken = this.authService.getfullNameFromToken();
+        this.fullName = val || fullNameFromToken;
+      },
+      error: (err) => {
+        console.error("Failed to fetch full name from store:", err);
+      }
+    });
+  
+    // Fetch and set the user's role
+    this.store.getRoleFromStore().subscribe({
+      next: (val) => {
+        const roleInToken = this.authService.getRoleFromToken();
+        this.role = val || roleInToken;
+      },
+      error: (err) => {
+        console.error("Failed to fetch role from store:", err);
+      }
+    });
+  }
+  
+
 
 }
