4e560eda237e2c30a6bf5da46e7d2755f1b2b416
althafhsn <althafhsn.c@gmail.com>
2024-11-20 16:42:10 +0530

    sef

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -54,7 +54,6 @@ export class AuthService {
   decodeToken(){
     const jwtHelper = new JwtHelperService();
     const token = this.getToken()!;
-    console.log(jwtHelper.decodeToken(token))
     return jwtHelper.decodeToken(token)
   }
   
