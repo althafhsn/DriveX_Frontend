6a5422eee114ff1f82d69891a6a7d5166fb9109b
althafhsn <althafhsn.c@gmail.com>
2024-11-20 23:40:29 +0530

    reset mail

---
+++ src/app/services/reset-password.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ResetPassword } from '../models/reset-password-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResetPasswordService {
+
+  private baseUrl: string = "http://localhost:5147/api/User/"
+
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  sendResetPasswordLink(email : string){
+    return this.http.post<any>(`${this.baseUrl}sent-reset-email/${email}`,{})
+  }
+  resetPassword(resetPasswordObj:ResetPassword){
+    return this.http.post<any>(`${this.baseUrl}reset-password`, resetPasswordObj)
+  }
+}
