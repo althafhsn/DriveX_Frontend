988ae78278e71140cc365180f772d87b81f2cc72
Nasra01547 <ut01547tic2024@gmail.com>
2024-11-27 17:23:43 +0530

    merge

--- src/app/components/dashboard/booking/booking.component.ts
+++ src/app/components/dashboard/booking/booking.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Booking } from '../../../models/booking.model';
+import { BookingService } from '../../../services/booking.service';
 
 @Component({
   selector: 'app-booking',
@@ -6,9 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './booking.component.css'
 })
 export class BookingComponent {
-  bookings = [
-    { id: '#5D869F5L2', customerName: 'Mason Wilson', pickupDropoff: 'San Diego - Dallas', dateTime: '15 Sept, 8:30AM', status: 'Confirmed' },
-    { id: '#3R7G8T9K1', customerName: 'Benjamin Anderson', pickupDropoff: 'New York - Los Angeles', dateTime: '20 Oct, 2:45PM', status: 'Cancelled' },
-    // Add more entries as needed
-  ];
+  selectedBooking: Booking | null = null;
 }
