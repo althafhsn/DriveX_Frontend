a038401c05999078a4b1f6caf4c7a90c05d23e20
Your NameHasniUT01624 <wingscer@gmail.com>
2024-11-29 10:38:59 +0530

    Somthing

--- src/app/components/Customer/landing-page/brand/brand.component.ts
+++ src/app/components/Customer/landing-page/brand/brand.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class BrandComponent {
   carBrands = [
     { name: 'Audi', image: 'IMAGE/audi.jpg' },
-    { name: 'BMW 1', image: 'https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg' },
+    { name: 'BMW', image: 'https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg' },
     { name: 'Mercedes Benz', image: 'https://upload.wikimedia.org/wikipedia/commons/9/90/Mercedes-Logo.svg' },
     { name: 'Tesla Motors', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg' },
     { name: 'Volkswagen', image: 'https://july.finestwp.com/newwp/carola/wp-content/uploads/2024/08/volkswagen.png' },
