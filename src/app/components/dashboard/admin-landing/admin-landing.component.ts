717f7d8d604f9c21b50efb3d84ac4f637a7dcd38
shahlakiyas <shahlamkfn@gmail.com>
2024-12-13 10:38:53 +0530

    notification in admin nav bar

--- src/app/components/dashboard/admin-landing/admin-landing.component.ts
+++ src/app/components/dashboard/admin-landing/admin-landing.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { UserStoreService } from '../../../services/user-store.service';
+import { BookingService } from '../../../services/booking.service';
 
 @Component({
   selector: 'app-admin-landing',
   templateUrl: './admin-landing.component.html',
   styleUrl: './admin-landing.component.css'
 })
-export class AdminLandingComponent {
+export class AdminLandingComponent implements OnInit{
+  public role:string | null = null;
+  public token:string | null = null;
+  rentalRequestCount: number=0;
+   constructor(
+     private authService: AuthService,
+     private store:UserStoreService,
+    private bookingService:BookingService
+   ) { }
+   
 
+  ngOnInit(): void {
+    this.getPendingRentalRequestCount();
+    // Fetch and set the user's role
+    this.store.getRoleFromStore().subscribe({
+      next: (val) => {
+        const roleInToken = this.authService.getRoleFromToken();
+        this.role = val || roleInToken;
+      },
+      error: (err) => {
+        console.error("Failed to fetch role from store:", err);
+      }
+    });
+
+    this.token = this.authService.getToken();
+    this.store.getRoleFromStore().subscribe({
+      next: (val) => {
+        this.role = val || this.authService.getRoleFromToken();
+      },
+      error: (err) => {
+        console.error("Failed to fetch role from store:", err);
+      }
+    });
+   
 }
+getPendingRentalRequestCount(): void {
+  this.bookingService.getBookings().subscribe(
+    (bookings) => {
+      console.log('API Response:', bookings); // Debugging log
+      if (Array.isArray(bookings) && bookings.length > 0) {
+        this.rentalRequestCount = bookings.filter(booking => booking.action === 'Pending').length;
+      }
+      console.log('Pending Rental Requests Count:', this.rentalRequestCount); // Check the count
+    },
+    (error) => {
+      console.error('Error fetching bookings', error);
+    }
+  );
+}
+}       
--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -11,7 +11,7 @@ export class BookingService {
 
   constructor(private http: HttpClient) {}
 
-  // Method to fetch all cars
+  
   getBookings(): Observable<Booking[]> {
     return this.http.get<Booking[]>(`${this.baseUrl}getAllRentalRequests`);
   }
